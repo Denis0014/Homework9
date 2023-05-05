078e786cb95c2b60cbd80eae7cc15c26bdf44af7
Denis <109104246+Denis0014@users.noreply.github.com>
2023-05-05 15:31:06 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/BinarySearchTree.cs
@@ -0,0 +1,70 @@
+﻿namespace BinaryTrees;
+public class BinarySearchTree
+{
+    /// <summary>
+    /// Создание экземпляра дерева
+    /// </summary>
+    private TreeNode<int> root;
+    /// <summary>
+    /// Добавляет целое в ДБП с сохранением упорядоченности
+    /// </summary>
+    /// <param name="node">Ссылка на корень</param>
+    /// <param name="x">Целое число</param>
+    void Add(TreeNode<int> node, int x)
+    {
+        if (node == null)
+        {
+            return;
+        }
+        if (node.Data >= x)
+        {
+            if (node.Left == null)
+            {
+                node.Left = new TreeNode<int>(x);
+                return;
+            }
+            Add(node.Left, x);
+        }
+        else
+        {
+            if (node.Right == null)
+            {
+                node.Right = new TreeNode<int>(x);
+                return;
+            }
+            Add(node.Right, x);
+        }
+    }
+    /// <summary>
+    /// Поиск целого в ДБП
+    /// </summary>
+    /// <param name="num">Целое число</param>
+    /// <returns>Найден элемент или нет</returns>
+    public bool Search(int num)
+    {
+        var node = root;
+        bool BinarySearch(TreeNode<int> node)
+        {
+            if (node == null)
+                return false;
+            if (node.Data == num)
+                return true;
+            return BinarySearch(num <= node.Data ? node.Left : node.Right);
+        }
+        return BinarySearch(root);
+    }
+    /// <summary>
+    /// Инициализирует дерево бинарного поиска по исходому массиву целых
+    /// </summary>
+    /// <param name="arr">массив целых чисел</param>
+    public BinarySearchTree(params int[] arr)
+    {
+        root = new TreeNode<int>(arr[0]);
+        for (int i = 1; i < arr.Length; i++)
+            Add(root, arr[i]);
+    }
+    /// <summary>
+    /// Печатает ДБП инфиксным обходом. Если дерево пустое, выводится &lt;empty tree&gt;
+    /// </summary>
+    public void Print() => TreeUtils.PrintTreeInfix(root);
+}
---
+++ ConsoleApp3/TreeNode.cs
@@ -0,0 +1,36 @@
+﻿namespace BinaryTrees;
+
+/// <summary>
+/// Узел бинарного дерева
+/// </summary>
+public class TreeNode<T>
+{
+    /// <summary>
+    /// Поле данных
+    /// </summary>
+    public T Data;
+
+    /// <summary>
+    /// Левое поддерево
+    /// </summary>
+    public TreeNode<T>? Left;
+
+    /// <summary>
+    /// Правое поддерево
+    /// </summary>
+    public TreeNode<T>? Right;
+
+    /// <summary>
+    /// Инициализирует узел бинарного дерева значением data поля данных
+    /// и поддеревьями left, right
+    /// </summary>
+    /// <param name="data">Значение поля данных узла</param>
+    /// <param name="left">Левое поддерево</param>
+    /// <param name="right">Правое поддерево</param>
+    public TreeNode(T data, TreeNode<T>? left = null, TreeNode<T>? right = null)
+    {
+        Data = data;
+        Left = left;
+        Right = right;
+    }
+}
